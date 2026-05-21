cdf7094a62d5715c7a3a5a17ae92fa52d1f1f4a7
FullSalesSystem <falecom@fullsalessystem.com>
2026-05-20 21:01:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+(function () {
+  var s = document.createElement("script");
+  s.src = "https://scripts.converteai.net/c678e70b-13db-47d3-b046-f3e247d16ff7/players/6843286d92e4f2ececa984de/v4/player.js";
+  s.async = true;
+  document.head.appendChild(s);
+})();
